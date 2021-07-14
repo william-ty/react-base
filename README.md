4615e779058983756dbb3d73385649105ae75c8a
william-ty <william.trahay@gmail.com>
2021-07-14 22:23:00 +0200

    [UPDATE] global style updated

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# react-base
-
-A simple react project used as a template
-
----
+# Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Portfolio from './components/portfolio/Portfolio';
 import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
-import "./App.scss"
+import "./app.scss"
 
 function App() {
   return (
