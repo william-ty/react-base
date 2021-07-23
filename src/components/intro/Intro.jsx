503696a84c4b1ad62e380b9371fb0fd58a49cd90
William Trahay <william.trahay@gmail.com>
2021-07-23 17:01:03 +0200

    [UPDATE]

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,21 +1,21 @@
 import "./intro.scss"
-import {init} from 'ityped'
+import { init } from 'ityped'
 import { useEffect, useRef } from "react"
 
 export default function Intro() {
-    
-    const textRef = useRef()
 
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: true,
-            backDelay: 1500,
-            backSpeed: 60,
-            strings: ["Developer", "Designer", "Music producer"],
-        })
-    }, [])
-    
-    console.log(textRef)
+    // const textRef = useRef()
+
+    // useEffect(() => {
+    //     init(textRef.current, {
+    //         showCursor: true,
+    //         backDelay: 1500,
+    //         backSpeed: 60,
+    //         strings: ["Developer", "Designer", "Music producer"],
+    //     })
+    // }, [])
+
+    // console.log(textRef)
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -27,7 +27,11 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>William Trahay</h1>
-                    <h3>Freelance <span ref={textRef}></span></h3>
+                    <h3>Freelance
+                        <span
+                            // ref={textRef}
+                        >
+                        </span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="" />
